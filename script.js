95d558dbd50e498860eaf4150797f8a42d223b8f
charlesmariga <charlesmariga37@gmail.com>
2021-10-26 10:11:27 +0300

    Finished asynchronous javascript

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const renderCountry = country => {
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${country.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${country.name}</h3>
+        <h4 class="country__region">${country.region}</h4>
+        <p class="country__row">
+          <span>👫</span>${(country.population / 1000000).toFixed(1)} M people
+        </p>
+        <p class="country__row">
+          <span>🗣️</span>${country.languages[0].name}
+        </p>
+        <p class="country__row">
+          <span>💰</span>${country.currencies[0].name}
+        </p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+
+const whereAmI = async function (country) {
+  const res = await fetch(`https://restcountries.com/v2/name/${country}`);
+  console.log(res);
+
+  const data = await res.json();
+  renderCountry(data[0]);
+};
+
+whereAmI('kenya');
